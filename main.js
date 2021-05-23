89a8974daed1f781312df27d12cfa6af25586212
Cristian <camilo.santamaria322@gmail.com>
2021-05-23 17:38:52 -0500

    Definicion de la bola, se agrego en la funcion draw un case para dibujar la bola, se creo una funcion que me ejecutara el clean y el draw en el view dejando el main con play,se definio la clase ball con sus propiedades

--- main.js
+++ main.js
@@ -29,6 +29,21 @@
     }
 })();
 
+//Ahora se creara la funcion de la pelota
+(function() {
+    self.Ball = function(x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.board = board;
+        this.speed_y = 0;
+        this.speed_x = 3;
+
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+
 //Se generara una nueva clase (Dibuja las barras)
 (function() {
     //Aca se le pasara la posicion(x,y) el alto y el ancho y el tablero
@@ -72,12 +87,19 @@
     }
 
     self.BoardView.prototype = {
+        clean: function() {
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+        },
         draw: function() {
             for (var i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
 
                 draw(this.ctx, el);
             };
+        },
+        play: function() {
+            this.clean();
+            this.draw();
         }
     }
 
@@ -85,45 +107,57 @@
     //Este dibujara los elementos
     function draw(ctx, element) {
         //Me dice si el objeto tiene una propiedad kind
-        if (element !== null && element.hasOwnProperty('kind')) {
-            switch (element.kind) {
-                case "rectangle":
-                    ctx.fillRect(element.x, element.y, element.width, element.height);
-                    break;
-            }
+
+        switch (element.kind) {
+            case "rectangle":
+                ctx.fillRect(element.x, element.y, element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
+
 })();
 
 //Creo el tablero
 var board = new Board(800, 400);
 //Creo la barra
 var bar = new Bar(20, 100, 40, 100, board);
-var bar = new Bar(735, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
+
 
 
 //Aca esta pendiente del keydown el cual es un evento de teclado sucede cuando se preciona una tecla
 document.addEventListener("keydown", function(ev) {
-    //Identificar la tecla
-    //console.log(ev.keyCode);
+    ev.preventDefault();
+    console.log(ev.keyCode);
     //aca inicializa con las teclas de arriba y la tecla hacia abajo
-    if (ev.keyCode == 38) {
+    if (ev.keyCode === 38) {
         bar.up();
-    } else if (ev.keyCode == 40) {
+    } else if (ev.keyCode === 40) {
         bar.down();
+    } else if (ev.keyCode === 87) {
+        bar_2.up();
+    } else if (ev.keyCode === 83) {
+        bar_2.down();
     }
     //Con esta linea podemos saber la coordenada con la que esta cambiando la barra
     //console.log(bar.toString()) = console.log(""+bar);
 });
 
 //Aca esta pendiente de que cargue la pagina cuando carga llama el metodo main
-self.addEventListener("load", main);
+//self.addEventListener("load", main);
 
+window.requestAnimationFrame(controller);
 //Este seria el Controlador (MVC)
-function main() {
-
-    board_view.draw();
-
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
