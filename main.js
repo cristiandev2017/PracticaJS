ceb5a475ac6f09651c4bd6db99bff0a6147e805f
Cristian <camilo.santamaria322@gmail.com>
2021-05-23 13:50:06 -0500

    Definicion de las barras (clase Bar), dibujar con draw las barras y usar propiedad rectangulo, Instancia de la barra creada en el main para uso como objeto

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Funcion anonima
+//Este es el MODELO (MVC)
 (function() {
     //Se define una variable como clase y en ella se crea un constructor
     self.Board = function(width, height) {
@@ -23,12 +23,39 @@
         get elements() {
             var elements = this.bars;
             //Se agrega una pelota
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
 })();
 
+//Se generara una nueva clase (Dibuja las barras)
+(function() {
+    //Aca se le pasara la posicion(x,y) el alto y el ancho y el tablero
+    self.Bar = function(x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+
+        //Accedo al vector(arreglo) que iniciamos arriba y le mandamos el objeto total osea la barra 
+        this.board.bars.push(this);
+        //Este es para decirle que elemento es para que canvas sepa como dibujarlo
+        this.kind = "rectangle";
+    }
+
+    self.Bar.prototype = {
+        down: function() {
+
+        },
+        up: function() {
+
+        }
+    }
+})();
+
+//Esta es la vista en el modelo (MVC)
 (function() {
     self.BoardView = function(canvas, board) {
         this.canvas = canvas;
@@ -38,13 +65,43 @@
         //Con este dibujaremos
         this.ctx = canvas.getContext("2d");
     }
+
+    self.BoardView.prototype = {
+        draw: function() {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
+                var el = this.board.elements[i];
+
+                draw(this.ctx, el);
+            };
+        }
+    }
+
+    //Aca implementaremos unas funciones las cuales no son de un objeto, pero que nos ayudan a realizar ciertas cosas
+    //Este dibujara los elementos
+    function draw(ctx, element) {
+        //Me dice si el objeto tiene una propiedad kind
+        if (element !== null && element.hasOwnProperty('kind')) {
+            switch (element.kind) {
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y, element.width, element.height);
+                    break;
+            }
+        }
+    }
 })();
 
 window.addEventListener("load", main);
 
+//Este seria el Controlador (MVC)
 function main() {
+    //Creo el tablero
     var board = new Board(800, 400);
+    //Creo la barra
+    var bar = new Bar(20, 100, 40, 100, board);
+    var bar = new Bar(735, 100, 40, 100, board);
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
+
+    board_view.draw();
 
 }
